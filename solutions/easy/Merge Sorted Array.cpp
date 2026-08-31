023e5173b4baa5992bbc5c1651f8f6bde40bbd87
Shivam <shivambhadana9391@gmail.com>
2026-09-01 00:44:10 +0530

    Add solution for Merge Sorted Array

---
+++ solutions/easy/Merge Sorted Array.cpp
@@ -0,0 +1,22 @@
+// Title: Merge Sorted Array
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/merge-sorted-array/
+
+            nums1[idx]=nums2[j];
+            idx--;
+            j--;
+            idx--;
+            i--;
+        }else{
+            nums1[idx]=nums1[i];
+        if(nums1[i]>=nums2[j]){
+       while(i>=0 && j>=0){
+       int j=n-1;
+       int idx=m+n-1;
+       int i=m-1;
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+public:
+        }
+       }
