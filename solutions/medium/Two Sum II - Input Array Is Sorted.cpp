6b106a045290b34c1fe3c7308bab723b21c8462b
Shivam <shivambhadana9391@gmail.com>
2026-08-31 23:06:54 +0530

    Add solution for Two Sum II - Input Array Is Sorted

---
+++ solutions/medium/Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,22 @@
+// Title: Two Sum II - Input Array Is Sorted
+            // Difficulty: Medium
+            // Language: C++
+            // Link: https://leetcode.com/problems/two-sum-ii---input-array-is-sorted/
+
+                return {l + 1, r + 1};
+            }
+            else if (sum < target) {
+                l++;
+            }
+            else {
+                r--;
+            if (sum == target) {
+
+            int sum = numbers[l] + numbers[r];
+
+        while (l < r) {
+        int l = 0;
+        int r = numbers.size() - 1;
+    vector<int> twoSum(vector<int>& numbers, int target) {
+class Solution {
+public:
