234b3a6b9fc7d78eb401426fdb3119184337c7db
Shivam <shivambhadana9391@gmail.com>
2026-09-02 15:49:36 +0530

    Add solution for Range Sum Query - Immutable

---
+++ solutions/easy/Range Sum Query - Immutable.cpp
@@ -0,0 +1,6 @@
+// Title: Range Sum Query - Immutable
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/range-sum-query---immutable/
+
+c
