cf2da9e37f3b8c9014f82ffbc82d4462d640a9da
Shivam <shivambhadana9391@gmail.com>
2026-09-08 23:05:25 +0530

    Add solution for Happy Number

---
+++ solutions/easy/Happy Number.cpp
@@ -0,0 +1,21 @@
+// Title: Happy Number
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/happy-number/
+
+            while (n > 0) {
+                int rem = n % 10;
+                sum += rem * rem;
+            int sum = 0;
+            s.insert(n);
+
+                return false;  
+            }
+
+        while (n != 1) {
+            if (s.count(n)) {
+        unordered_set<int> s;
+
+public:
+    bool isHappy(int n) {
+class Solution {
